360a0ea7205694d60329739749cdc7e506d2c83d
XodTuza <gordie2u@gmail.com>
2021-02-15 19:52:04 +0100

    forgot password

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/app.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../shared/services/auth.service';
+import {MessageService} from '../../shared/services/message.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,7 +12,22 @@ import { AppService } from 'src/app/app.service';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor(appService: AppService) { }
+  forgotpasswordForm: FormGroup;
+
+  connecting = {
+    creating: false
+  };
+
+  connectingErrors = {
+    creating: false
+  };
+
+
+  constructor(appService: AppService, private authService: AuthService,  private router: Router, private messageService: MessageService) { 
+    this.forgotpasswordForm = new FormGroup({
+      email: new FormControl(null, Validators.email),
+    });
+  }
 
   pagesJs = [
     'assets/js/jquery-3.5.0.min.js',
@@ -21,6 +40,34 @@ export class ForgotPasswordComponent implements OnInit {
 
   ngOnInit(): void {
     AppService.loadScriptPage(this.pagesJs);
-  } 
+  }
+  
+  isValid(controlName) {
+    return this.forgotpasswordForm.get(controlName).invalid && this.forgotpasswordForm.get(controlName).touched;
+  }
+
+  forgotpassword() {
+
+    if (this.forgotpasswordForm.invalid) {
+      this.messageService.warning('You\'re missing something important', null);
+      return;
+    }
+
+    this.connecting.creating = true;
+
+    if (this.forgotpasswordForm.valid) {
+      this.authService.submitForgotpassword(this.forgotpasswordForm.value).subscribe(
+        (data: any) => {
+          this.messageService.success('Email Sent', null);
+          this.router.navigate(['password-request-sent']).then();
+        },
+        error => {
+          this.messageService.error('Something went wrong', 'Error!');
+          this.connecting.creating = false;
+          this.connectingErrors.creating = true;
+        }
+      );
+    }
+  }
 
 }
